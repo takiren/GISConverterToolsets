857163c6424dc0ef615a510a881bef1daad0e325
taki <tyorokyuu@gmail.com>
2023-04-22 12:42:45 +0900

    bug fixed

--- GMLtoGeoTIFF/main.cpp
+++ GMLtoGeoTIFF/main.cpp
@@ -34,11 +34,15 @@ int main(int argc, char* argv[]) {
     GDALAllRegister();
     CPLPushErrorHandler(CPLQuietErrorHandler);
     bool blist = false;
+    bool combine = false;
     try {
       options.add_options()("l,list", "list source files",
-                            cxxopts::value<bool>()->default_value("false"));
+                            cxxopts::value<bool>()->default_value("false"))(
+          "c,combine", "Combine geotiffs",
+          cxxopts::value<bool>()->default_value("false"));
       auto result = options.parse(argc, argv);
       blist = result["list"].as<bool>();
+      combine = result["combine"].as<bool>();
 
     } catch (cxxopts::OptionException& e) {
       std::cout << options.usage() << std::endl;
@@ -47,23 +51,21 @@ int main(int argc, char* argv[]) {
     }
 
     vector<fs::path> sources;
-    std::string slist;
-
-    if (argc > 1) {
-      slist = argv[1];
-      if (slist == string("list")) blist = true;
-    }
 
     for (const auto& it :
          fs::recursive_directory_iterator(fs::current_path().append("gmls"))) {
       if (!it.is_directory()) {
         if (it.path().extension() == ".xml") {
           sources.push_back(it.path());
-          if (blist) cout << it.path().filename() << endl;
         }
       }
     }
-    if (blist) return 0;
+    if (blist) {
+      for (auto& e : sources) {
+        cout << e.filename() << endl;
+      }
+      return 0;
+    }
 
     GDALDriver* gdriver = nullptr;
     gdriver = GetGDALDriverManager()->GetDriverByName("GTiff");
